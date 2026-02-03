cfcaa76835a7d8bc525991e97a3b6589ba1b5c67
Alexandre Boucher <albou631@ulaval.ca>
2026-02-03 18:37:06 -0500

    1.2 Exercices (3)

--- equipe240.py
+++ equipe240.py
@@ -9,3 +9,10 @@ a = np.full((6, 1), 1) # ok
 b = np.arange(7) # ok
 c = a.reshape(6) # ok
 d = c * 240 # ok
+I = np.identity(6) # ok
+J = np.full((6, 6), 1) # ok
+K = np.diag(b) # ok
+L = I * 55 - J + 2*(a*c) # ok (vérifié à quoi est supposé ressembler la matrice)
+M = K
+M[:, 0] = a.reshape((6,1))
+print(M)
