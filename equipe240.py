1853e36a3954bd3d47006be08e2a9cf53c7da88d
Alexandre Boucher <albou631@ulaval.ca>
2026-02-03 21:07:40 -0500

    1.2 Exercices (4)

--- equipe240.py
+++ equipe240.py
@@ -6,13 +6,38 @@ from suiteSn import suiteSn
 
 # 1.2 Exercices:
 a = np.full((6, 1), 1) # ok
-b = np.arange(7) # ok
+print(a)
+
+b = np.arange(1, 7) # ok
+print(b)
+
 c = a.reshape(6) # ok
+print(c)
+
 d = c * 240 # ok
+print(d)
+
 I = np.identity(6) # ok
+print(I)
+
 J = np.full((6, 6), 1) # ok
+print(J)
+
 K = np.diag(b) # ok
+print(K)
+
 L = I * 55 - J + 2*(a*c) # ok (vérifié à quoi est supposé ressembler la matrice)
-M = K
-M[:, 0] = a.reshape((6,1))
+print(L)
+
+M = K ; M[:, 0] = a.reshape((6,)) # ok
 print(M)
+
+dd = np.linalg.det(M) # ok
+print(dd)
+
+x = np.linalg.solve(M, a) # ok (vérifié avec réponse attendu)
+print(x)
+
+N = np.linalg.solve(M, M.T) # ok (M.T est un attribut (variable d'instance) des objet Numpy (la version transporté))
+print(N)
+
