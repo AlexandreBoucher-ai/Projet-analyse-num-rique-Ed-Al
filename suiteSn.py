6fe7cf4527be0c9aff317af250359b4e1a9a1da3
Alexandre Boucher <albou631@ulaval.ca>
2026-02-03 22:16:24 -0500

    2 b) (2)

--- suiteSn.py
+++ suiteSn.py
@@ -5,8 +5,9 @@ def suiteSn(n):
     # on innitialise un array 1 seul temer pour l'instant, S0
     S = np.array([e-1])
     # on veut construire un tableau de n+1 élément, donc i prend n valeur et il y aura n itération
-    for i in range(n):
-        S = np.append(S, np.exp(1) - n * S[-1])
+    # doit commencer à 1 car sinon tout est décalé, et on donc donc ajouté +1 à n aussi
+    for i in range(1, n+1):
+        S = np.append(S, np.exp(1) - i * S[-1])
     return S
-    
+# vérifier si résultat est bon    
 print(suiteSn(10))
